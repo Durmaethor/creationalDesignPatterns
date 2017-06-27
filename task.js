35c6f5784e7ae9e50127948713a4ace16b62fd62
Ryan Kramer <RKramer613@gmail.com>
2017-06-27 13:53:59 -0700

    Separated code into different files, one for task, other for object creation

--- task.js
+++ task.js
@@ -12,12 +12,4 @@ Task.prototype.save = function() {
     console.log('Saving Task: ' + this.name);
 };
 
-var task1 = new Task('Create a demo for constructors');
-var task2 = new Task('Create a demo for modules');
-var task3 = new Task('Create a demo for singletons');
-var task4 = new Task('Create a demo for prototypes');
 
-task1.complete();
-task2.save();
-task3.save();
-task4.save();
