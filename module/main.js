0f80fbe76ad92c2a333904918951b7743ea860ee
Ryan Kramer <RKramer613@gmail.com>
2017-06-27 15:56:17 -0700

    changed tasks to objects, testing code

--- module/main.js
+++ module/main.js
@@ -5,9 +5,9 @@ var repo = require('./taskRepository');
 
 var task1 = new Task(repo.get(1));
 
-var task2 = new Task('Create a demo for modules');
-var task3 = new Task('Create a demo for singletons');
-var task4 = new Task('Create a demo for prototypes');
+var task2 = new Task({name:'Create a demo for modules'});
+var task3 = new Task({name:'Create a demo for singletons'});
+var task4 = new Task({name:'Create a demo for prototypes'});
 
 task1.complete();
 task2.save();
