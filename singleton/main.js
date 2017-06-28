2dcdac1ff3d13ae34cc6d53c89572b2db40432a3
Ryan Kramer <RKramer613@gmail.com>
2017-06-28 12:33:00 -0700

    Notes on what this does

--- singleton/main.js
+++ singleton/main.js
@@ -10,3 +10,16 @@ taskHandler.save();
 taskHandler.save();
 taskHandler.save();
 taskHandler.save();
+
+// As a SINGLETON this will now log the following:
+
+// Newing up the task repo
+// Saving fromMain Called 1 times
+// Saving fromMain Called 2 times
+// Saving fromMain Called 3 times
+// Saving Hi from taskHandler Called 4 times
+// Saving Hi from taskHandler Called 5 times
+// Saving Hi from taskHandler Called 6 times
+// Saving Hi from taskHandler Called 7 times
+
+// As you can see, with singleton it only runs the functions once, and does not duplicate. Rather, it addes them up. (count++)
