495446424d7a097e86a984d36d421b6aca62e68d
Ryan Kramer <RKramer613@gmail.com>
2017-06-29 13:14:51 -0700

    Tested and posted results in notes

--- behavioral/command/taskRepo.js
+++ behavioral/command/taskRepo.js
@@ -74,3 +74,23 @@ console.log(repo.tasks);
 
 repo.replay();
 console.log(repo.tasks);
+
+// Running results:
+
+        // Saving Task 1 to the db
+        // Saving Task 2 to the db
+        // Saving Task 3 to the db
+        // Saving Task 4 to the db
+        // { '1': { id: 1, name: 'Task 1', completed: false },
+        //   '2': { id: 2, name: 'Task 2', completed: false },
+        //   '3': { id: 3, name: 'Task 3', completed: false },
+        //   '4': { id: 4, name: 'Task 4', completed: false } }
+        // {}
+        // Saving Task 1 to the db
+        // Saving Task 2 to the db
+        // Saving Task 3 to the db
+        // Saving Task 4 to the db
+        // { '1': { id: 1, name: 'Task 1', completed: false },
+        //   '2': { id: 2, name: 'Task 2', completed: false },
+        //   '3': { id: 3, name: 'Task 3', completed: false },
+        //   '4': { id: 4, name: 'Task 4', completed: false } }
