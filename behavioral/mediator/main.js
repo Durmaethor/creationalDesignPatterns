77e42fd052bce680941aa18af35b2cf04be0a82a
Ryan Kramer <RKramer613@gmail.com>
2017-06-29 12:35:57 -0700

    Added main file and services code

---
+++ behavioral/mediator/main.js
@@ -0,0 +1,28 @@
+
+var Task = require('./task');
+
+var notificationService = function() { // This is an OBSERVER 
+    var message = 'Notifying ';
+    this.update = function (task) {
+        console.log(message + task.user + ' for task ' + task.name);
+    }
+}
+
+var loggingService = function() {  // This is an OBSERVER 
+    var message = 'Logging ';
+    this.update = function (task) {
+        console.log(message + task.user + ' for task ' + task.name);
+    }
+}
+
+var auditingService = function() { // This is an OBSERVER
+    var message = 'Auditing ';
+    this.update = function (task) {
+        console.log(message + task.user + ' for task ' + task.name);
+    }
+}
+
+var task1 = new Task({
+    name: 'Create a demo for constructors', 
+    user: 'Ryan'
+});
