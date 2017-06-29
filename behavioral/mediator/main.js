63fbf2b99e3135ac9781359575dd3ee3953eed62
Ryan Kramer <RKramer613@gmail.com>
2017-06-29 12:46:10 -0700

    Created Mediator

--- behavioral/mediator/main.js
+++ behavioral/mediator/main.js
@@ -21,7 +21,39 @@ var auditingService = function() { // This is an OBSERVER
     this.update = function (task) {
         console.log(message + task.user + ' for task ' + task.name);
     }
-}
+};
+
+var mediator = (function(){
+    var channels = {};
+
+    var subscribe = function(channel, context, func) {
+       if(!mediator.channels[channel]) {
+           mediator.channels[channel] = []
+       } 
+       mediator.channels[channel].push({
+           context: context, 
+           func: func
+       });
+    };
+
+    var publish = function(channel) {
+        if(!this.channels[channel]) {
+            return false
+        }
+        var args = Array.prototype.slice.call(arguments, 1);
+
+        for(var i = 0; i < mediator.channels[channel].length; i++) {
+            var sub = mediator.channels[channel][i];
+            sub.func.apply(sub.context, args)
+        }
+    }
+    return {
+        channels: {},
+        subscribe: subscribe,
+        publish: publish
+    };
+
+}());
 
 // create new task
 var task1 = new Task({
