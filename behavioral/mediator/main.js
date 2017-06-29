141e9b535f5ea1132425f0cfdde1e3595ae9ec5f
Ryan Kramer <RKramer613@gmail.com>
2017-06-29 12:38:03 -0700

    Notes

--- behavioral/mediator/main.js
+++ behavioral/mediator/main.js
@@ -1,6 +1,7 @@
 
 var Task = require('./task');
 
+// Service notifications and updates
 var notificationService = function() { // This is an OBSERVER 
     var message = 'Notifying ';
     this.update = function (task) {
@@ -22,7 +23,13 @@ var auditingService = function() { // This is an OBSERVER
     }
 }
 
+// create new task
 var task1 = new Task({
     name: 'Create a demo for constructors', 
     user: 'Ryan'
 });
+
+// invoke instances of the 3 services
+var not = new notificationService();
+var ls = new loggingService();
+var audit = new auditingService();
