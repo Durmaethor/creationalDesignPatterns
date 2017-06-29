be22d45c3c9fff2936a21d0c4a458e57bd5625d3
Ryan Kramer <RKramer613@gmail.com>
2017-06-29 10:21:12 -0700

    Created TaskServiceWrapper to clean up code (facade)

--- facade/task.js
+++ facade/task.js
@@ -26,6 +26,22 @@ var TaskService = function () {
     }
 }();
 
+var TaskServiceWrapper = function() {
+
+    var completeAndNotify = function (task) {
+        TaskService.complete(myTask);
+            if(myTask.completed === true) {
+                TaskService.setCompleteDate(myTask);
+                TaskService.notifyCompletion(myTask, myTask.user);
+                TaskService.save(myTask);
+        }  
+    }
+    
+    return {
+        completeAndNotify: completeAndNotify
+    }
+}
+
 var myTask = new Task ({
     name: 'MyTask',
     priority: 1,
