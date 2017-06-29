53431f00ec2c5de3551bf4e605d9ae8df0022cd2
Ryan Kramer <RKramer613@gmail.com>
2017-06-29 11:55:16 -0700

    Added notification function

--- behavioral/main.js
+++ behavioral/main.js
@@ -1,6 +1,13 @@
 
 var Task = require('./task');
 
+var notificationService = function() {
+    var message = 'Notifying ';
+    this.update = function (task) {
+        console.log(message + task.user + ' for task ' + task.name);
+    }
+}
+
 var task1 = new Task({name: 'Create a demo for constructors', user: 'Ryan'});
 
 task1.save();
