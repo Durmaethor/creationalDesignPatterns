58e63a2f165277c169d95184da3c6818fee4c741
Ryan Kramer <RKramer613@gmail.com>
2017-06-29 13:00:25 -0700

    Setup task repo code

---
+++ behavioral/command/taskRepo.js
@@ -0,0 +1,13 @@
+
+var repo = {
+    get: function(id) {
+        console.log('Getting task ' + id);
+        return {
+            name: 'New task from db'
+        }
+    },
+    save: function(task) {
+        console.log('Saving ' + task.name + ' to the db');
+    }
+    
+}
