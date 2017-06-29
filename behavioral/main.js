abc9cb99dc03b3491611a400094f1ce702a99897
Ryan Kramer <RKramer613@gmail.com>
2017-06-29 11:57:01 -0700

    Added logging and auditing

--- behavioral/main.js
+++ behavioral/main.js
@@ -8,6 +8,20 @@ var notificationService = function() {
     }
 }
 
+var loggingService = function() {
+    var message = 'Logging ';
+    this.update = function (task) {
+        console.log(message + task.user + ' for task ' + task.name);
+    }
+}
+
+var auditingService = function() {
+    var message = 'Auditing ';
+    this.update = function (task) {
+        console.log(message + task.user + ' for task ' + task.name);
+    }
+}
+
 var task1 = new Task({name: 'Create a demo for constructors', user: 'Ryan'});
 
 task1.save();
